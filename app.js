3e7973b2460b3074a29b0479126ada25f01d5237
pufex <j.abram1@wp.pl>
2024-02-02 15:05:15 +0100

    added hamburger menu for mobile

---
+++ app.js
@@ -0,0 +1,15 @@
+const header = document.querySelector("header");
+const hamburger = document.querySelector(".hamburger")
+const hamburgerIcon = document.querySelector("#hamburger-icon")
+const closeIcon = document.querySelector("#close-icon")
+const hamburgerList = document.querySelector(".hamburger-links")
+const messageBox = document.querySelector(".message-box")
+
+hamburger.addEventListener('click', ()=>{
+    header.classList.toggle('hamburger-active-background');
+    hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+    hamburgerList.classList.toggle('hidden');
+    hamburgerList.classList.toggle('hamburger-list-active');
+    messageBox.classList.toggle('hidden');
+})
